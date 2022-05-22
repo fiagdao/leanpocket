f672f5a5fc0935a6dded4b69d3c879d9b8c9ffa3
poktblade <baaspoolsllc@gmail.com>
2022-05-21 23:23:28 -0500

    Change to RW lock for chains

--- app/config.go
+++ app/config.go
@@ -675,7 +675,7 @@ func NewHostedChains(generate bool) *types.HostedBlockchains {
 	// return the map
 	return &types.HostedBlockchains{
 		M: m,
-		L: sync.Mutex{},
+		L: sync.RWMutex{},
 	}
 }
 
@@ -711,7 +711,7 @@ func generateChainsJson(chainsPath string) *types.HostedBlockchains {
 		m[chain.ID] = chain
 	}
 	// return the map
-	return &types.HostedBlockchains{M: m, L: sync.Mutex{}}
+	return &types.HostedBlockchains{M: m, L: sync.RWMutex{}}
 }
 
 const (
